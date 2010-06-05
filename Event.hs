539a257aab77d272969e48ba9a67d5ed9a2c3ae7
Karl Voelker <ktvoelker@gmail.com>
2010-06-05 04:15:46 -0400

    Some work on Event module

--- Event.hs
+++ Event.hs
@@ -5,15 +5,41 @@ import Declare
 import State
 import X11
 
--- TODO
+import Data.Bits
+import Foreign.Marshal.Alloc
+
 addStdEvents :: Window -> X11 ()
-addStdEvents _ = return ()
+addStdEvents win = do
+  disp <- display
+  liftIO $ grabButton
+    disp anyButton anyModifier win True
+    buttonPressMask grabModeSync grabModeAsync none none
 
--- TODO
 addRootEvents :: Window -> X11 ()
-addRootEvents _ = return ()
+addRootEvents win = do
+  disp <- display
+  liftIO $ selectInput disp win
+    (resizeRedirectMask .|. substructureRedirectMask .|. substructureNotifyMask)
+
+eventLoop :: Config -> State -> X11 ()
+eventLoop conf state = do
+  ptr <- mallocBytes 96
+  until (???) handler (???)
+  free ptr
 
 -- TODO
-eventLoop :: Config -> State -> X11 ()
-eventLoop _ _ = return ()
+-- Add another monad, the X11State monad, which is:
+--  type X11State a = StateT State X11 a
+-- Add another constructor to the State type: Quit.
+-- Make the event handler use this monad between the malloc and free.
+--   It will loop until the state is Quit.
+
+{-
+loop :: Display -> (Display -> XEventPtr -> IO ()) -> IO ()
+loop disp h = allocaXEvent
+  (sequence_ . repeat . (\p -> nextEvent disp p >> h disp p))
+
+safeHandler :: (Display -> Event -> IO ()) -> Display -> XEventPtr -> IO ()
+safeHandler h disp ptr = getEvent ptr >>= h disp
+-}
 
