8ee84f162fa9bf91ef34068e78dfacbde8e01612
Karl Voelker <ktvoelker@gmail.com>
2010-07-13 22:53:14 -0400

    Added separate event handler functions for each event type

--- src/Event.hs
+++ src/Event.hs
@@ -14,15 +14,19 @@ import Graphics.X11.Xlib.Extras
 addStdEvents :: Window -> X11 ()
 addStdEvents win = do
   disp <- display
-  liftIO $ grabButton
-    disp anyButton anyModifier win True
-    buttonPressMask grabModeSync grabModeAsync none none
+  liftIO $ do
+    grabButton
+      disp anyButton anyModifier win True
+      (buttonPressMask .|. buttonReleaseMask)
+      grabModeSync grabModeAsync none none
+    selectInput disp win (keyPressMask .|. keyReleaseMask)
 
 addRootEvents :: Window -> X11 ()
 addRootEvents win = do
   disp <- display
   liftIO $ selectInput disp win
-    (resizeRedirectMask .|. substructureRedirectMask .|. substructureNotifyMask)
+    (resizeRedirectMask .|.
+     substructureRedirectMask .|. substructureNotifyMask)
 
 eventLoop :: Config -> World -> X11 ()
 eventLoop conf world = do
@@ -34,7 +38,36 @@ eventLoop conf world = do
 safe :: XEventPtr -> (Event -> X11State a) -> X11State a
 safe ptr = ((lift . lift . lift) (getEvent ptr) >>=)
 
--- TODO
-handler :: Config -> Event -> X11State [Action]
-handler _ _ = quitState
+handlers :: Map Graphics.X11.EventType (Config -> Event -> X11State [Action])
+handlers = fromList
+  [ (keyPress,      keyPressHandler)
+  , (keyRelease,    keyReleaseHandler)
+  , (buttonPress,   buttonPressHandler)
+  , (buttonRelease, buttonReleaseHandler)
+  , (resizeRequest, resizeRequestHandler)
+  , (mapNotify,     mapNotifyHandler)
+  , (mapRequest,    mapRequestHandler)
+  ]
+
+handler, keyPressHandler, keyReleaseHandler, buttonPressHandler,
+  buttonReleaseHandler, resizeRequestHandler, mapNotifyHandler,
+  mapRequestHandler, defaultHandler :: Config -> Event -> X11State [Action]
+
+handler c e = findWithDefault defaultHandler (ev_event_type e) handlers c e
+
+defaultHandler _ _ = quitState
+
+keyPressHandler = defaultHandler
+
+keyReleaseHandler = defaultHandler
+
+buttonPressHandler = defaultHandler
+
+buttonReleaseHandler = defaultHandler
+
+resizeRequestHandler = defaultHandler
+
+mapNotifyHandler = defaultHandler
+
+mapRequestHandler = defaultHandler
 
