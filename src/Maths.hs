9cba181425988b35313c6ec23b57533463d4a519
Karl Voelker <ktvoelker@gmail.com>
2010-07-24 01:59:25 -0400

    Maths library works

--- src/Maths.hs
+++ src/Maths.hs
@@ -1,8 +1,8 @@
 
-module Maths where
+module Maths (Pix, Cel, Posn, Span, Diff, Wrapper(..), Add(..), Sub(..)) where
 
-data Pixel
-data Cell
+data Pix
+data Cel
 
 newtype Posn t a = Posn a
 newtype Span t a = Span a
@@ -12,27 +12,15 @@ class Wrapper a where
   unwrap :: a b -> b
   wrap   :: b -> a b
 
-instance Wrapper (Posn Pixel) where
+instance Wrapper (Posn t) where
   unwrap (Posn n) = n
   wrap = Posn
 
-instance Wrapper (Span Pixel) where
+instance Wrapper (Span t) where
   unwrap (Span n) = n
   wrap = Span
 
-instance Wrapper (Diff Pixel) where
-  unwrap (Diff n) = n
-  wrap = Diff
-
-instance Wrapper (Posn Cell) where
-  unwrap (Posn n) = n
-  wrap = Posn
-
-instance Wrapper (Span Cell) where
-  unwrap (Span n) = n
-  wrap = Span
-
-instance Wrapper (Diff Cell) where
+instance Wrapper (Diff t) where
   unwrap (Diff n) = n
   wrap = Diff
 
@@ -50,9 +38,35 @@ class Sub a b c where
   infixl 6 -.
   (-.) :: a -> b -> c
 
+{-
+ - posn + posn -> invalid
+ - posn - posn -> span or diff
+ - posn + span -> posn
+ - posn - span -> posn
+ - span + span -> span
+ - span - span -> span or diff
+ - diff + diff -> invalid for now (could be diff)
+ - diff - diff -> invalid for now (could be diff)
+ -}
+
 instance (Num a) => Sub (Posn t a) (Posn t a) (Span t a) where
   (-.) = anyAbsMinus
 
 instance (Num a) => Sub (Posn t a) (Posn t a) (Diff t a) where
   (-.) = anyAbsMinus
+
+instance (Num a) => Add (Posn t a) (Span t a) (Posn t a) where
+  (+.) = anyPlus
+
+instance (Num a) => Sub (Posn t a) (Span t a) (Posn t a) where
+  (-.) = anyMinus
+
+instance (Num a) => Add (Span t a) (Span t a) (Span t a) where
+  (+.) = anyPlus
+
+instance (Num a) => Sub (Span t a) (Span t a) (Span t a) where
+  (-.) = anyAbsMinus
+
+instance (Num a) => Sub (Span t a) (Span t a) (Diff t a) where
+  (-.) = anyMinus
 
