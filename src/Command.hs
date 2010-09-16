ed4418a10b13cd0132003a7286a6b10bdfbdacec
Karl Voelker <ktvoelker@gmail.com>
2010-09-15 20:19:34 -0500

    Implemented CSeq

--- src/Command.hs
+++ src/Command.hs
@@ -13,6 +13,7 @@ runCommand :: Command -> X11State ()
 runCommand cmd = do
   dprint cmd
   case cmd of
+    (CSeq xs)       -> mapM_ runCommand xs
     (CFocusDir dir) -> focusDir dir
     (CExec x)       -> exec x
     _               -> return ()
