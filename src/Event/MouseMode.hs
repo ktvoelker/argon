e2cdc416afae827061cf590c72ccd2666e4f4ac8
Karl Voelker <ktvoelker@gmail.com>
2010-09-21 21:00:03 -0500

    Finished resize mode

--- src/Event/MouseMode.hs
+++ src/Event/MouseMode.hs
@@ -24,20 +24,21 @@ motionNotifyHandler e = do
   case wMode wo of
     MNormal     -> return ()
     m@MMouse {} -> do
+      -- Calculate the difference between the current and previous mouse
+      -- positions.
+      let (x, y) = mPosn m
+      let xy'@(x', y') = posnXY (ev_x e) (ev_y e)
+      let (dx, dy) = (x' -. x, y' -. y)
+      -- Record the current mouse position.
+      modifyWorld $ $(upd 'wMode) $ $(upd 'mPosn) $ const xy'
       case mMode m of
-        MMMove   -> moveHandler m e
-        MMResize -> resizeHandler m e
+        MMMove   -> moveHandler m e dx dy
+        MMResize -> resizeHandler m e dx dy
 
-moveHandler, resizeHandler :: Mode -> Event -> X11State ()
+moveHandler, resizeHandler
+  :: Mode -> Event -> Diff Pix X -> Diff Pix Y -> X11State ()
 
-moveHandler m e = do
-  -- Calculate the difference between the current and previous mouse
-  -- positions.
-  let (x, y) = mPosn m
-  let xy'@(x', y') = posnXY (ev_x e) (ev_y e)
-  let (dx, dy) = (x' -. x, y' -. y)
-  -- Record the current mouse position.
-  modifyWorld $ $(upd 'wMode) $ $(upd 'mPosn) $ const xy'
+moveHandler m e dx dy = do
   -- Get the window's current position.
   disp <- getDisplay
   (_, wx, wy, _, _, _, _) <- liftIO $ getGeometry disp win
@@ -47,5 +48,13 @@ moveHandler m e = do
   where
     win = ev_window e
 
-resizeHandler _ = defaultHandler
+resizeHandler m e dx dy = do
+  -- Get the window's current size.
+  disp <- getDisplay
+  (_, _, _, ww, wh, _, _) <- liftIO $ getGeometry disp win
+  -- Resize the window by the difference.
+  let (ww', wh') = (ww + unwrap dx, wh + unwrap dy)
+  liftIO $ resizeWindow disp win ww' wh'
+  where
+    win = ev_window e
 
