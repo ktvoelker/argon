f858af26c156659b2aa720f04014e68e3a302039
Karl Voelker <ktvoelker@gmail.com>
2010-10-20 17:04:13 -0500

    Make debug.sh more automatic

--- src/debug.sh
+++ src/debug.sh
@@ -4,13 +4,9 @@ cp -ru ../dist/build/argon/argon-tmp/* .
 
 # Make sure that ghci loads the targets in interpreted mode
 # so that breakpoints can be set.
-#
-# The sleep is necessary because otherwise the touch time
-# might be the same as the build-completion time.
-sleep 1
-touch $*
+find ./ -name '*.hs' | xargs -n1 touch
 
-exec ghci \
+DISPLAY=:1 exec ghci \
   -XDisambiguateRecordFields \
   -XRecordWildCards \
   -XTemplateHaskell \
