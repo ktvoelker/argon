37a41c679a2211868c34c54595dc8eb222ccc13f
Karl Voelker <ktvoelker@gmail.com>
2011-04-14 19:15:40 -0500

    Added "enter" to Fields module

--- src/Fields.hs
+++ src/Fields.hs
@@ -1,6 +1,7 @@
 
-module Fields (upd) where
+module Fields (upd, enter) where
 
+import Control.Monad.State
 import Language.Haskell.TH
 import Prelude
 
@@ -12,8 +13,27 @@ upd n = do
     LamE [VarP p0, VarP p1] $
     RecUpdE (VarE p1) [(n, AppE (VarE p0) (AppE (VarE n) (VarE p1)))]
 
+enter :: Name -> Q Exp
+enter n = do
+  s <- newName "s"
+  v <- newName "v"
+  r <- newName "r"
+  w <- newName "w"
+  t <- newName "t"
+  let u = return (n, VarE w)
+  let body = [| runState $(varE s) $(varE v) |]
+  lamE [varP s] $ doE 
+    [ bindS (varP v) [| gets $(varE n) |]
+    , letS [valD (tupP [varP r, varP w]) (normalB body) []]
+    , noBindS [| modify $(lamE [varP t] $ recUpdE (varE t) [u]) |]
+    , noBindS [| return $(varE r) |]
+    ]
+
 data Foo = Foo { foo :: Int }
 
 upd' :: IO ()
 upd' = runQ (upd 'foo) >>= print
+
+enter' :: IO ()
+enter' = runQ (enter 'foo) >>= print
 
